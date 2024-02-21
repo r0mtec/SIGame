be800b15bbc0d2a47ecf54838c139eb2fc1d203a
r0mtec <buslaevromtek@gmail.com>
2024-02-21 16:43:16 +0400

    Сreated a starting form and made the transition to the next one

---
+++ SignGame/SignGame/ChoseGameForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SignGame
+{
+    public partial class ChoseGameForm : Form
+    {
+        /// <summary>
+        /// Поле-обьект для работы с пользователем
+        /// </summary>
+        private ManageUser manageUser;
+
+        /// <summary>
+        /// Инициализация формы и поля-объекта
+        /// </summary>
+        /// <param name="manageUser">поле-объект</param>
+        public ChoseGameForm(ManageUser manageUser)
+        {
+            InitializeComponent();
+            this.manageUser = manageUser;
+        }
+
+        /// <summary>
+        /// При нажатии на кнопку изменяется имя Labal
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TempName.Text = manageUser.User?.Name;
+        }
+    }
+}
