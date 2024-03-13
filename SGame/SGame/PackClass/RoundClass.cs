20d420ea72a589e3e5dea9d967c631019f270695
funa4i <96429076+funa4i@users.noreply.github.com>
2024-03-13 19:22:11 +0400

    Чтение вопросов + основа всех укровней классов

---
+++ SGame/SGame/PackClass/RoundClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGame.PackClass
+{
+    public class RoundClass
+    {
+        public List<ThemesClass> questionClasses = new List<ThemesClass>();
+    }
+}
